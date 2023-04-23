bd142510e0dd8e8e1a78568d0fd231eaa0d5ef2a
Cupid Valentine <westerngod@126.com>
2023-04-23 23:14:33 +0800

    refactor: optimized router code

---
+++ cmd/cmd.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	return r
+}
+
+func RunServer() {
+	r := setupRouter()
+	r.Run()
+}
--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	return r
-}
+import "mangosteen/cmd"
 
 func main() {
-	r := setupRouter()
-	r.Run()
+	cmd.RunServer()
 }
