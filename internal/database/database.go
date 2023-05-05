e7e15b78a6a207a72767fa90ec0a06e7febc8ade
Cupid Valentine <westerngod@126.com>
2023-05-05 23:46:24 +0800

    feat: migrate tables with gorm

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,8 +1,88 @@
 package database
 
 import (
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 // var DB *sql.DB
+
 var DB *gorm.DB
+
+const (
+	host     = "127.0.0.1"
+	post     = 5432
+	user     = "mangosteen"
+	password = "123456"
+	dbname   = "mangosteen_dev"
+)
+
+// Connect for connecting the database
+func Connect() {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, post, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	DB = db
+}
+
+func Close() {
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sqlDB.Close()
+}
+
+type User struct {
+	ID        int
+	Email     string `gorm:"uniqueIndex"`
+	Phone     string
+	Address   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Item struct {
+	ID         int
+	UserID     int
+	Amount     int
+	HappenedAt time.Time
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+type Tag struct {
+	ID   int
+	Name string
+}
+
+var models = []any{&User{}, &Item{}, &Tag{}}
+
+// CreateTables for creating tables
+func CreateTables() {
+	for _, model := range models {
+		err := DB.Migrator().CreateTable(model)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	// err := DB.Migrator().CreateTable(&User{}, &Item{})
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	log.Println("Successfully create table")
+}
+
+func Migrate() {
+	DB.AutoMigrate(models...)
+}
+
+func Crud() {
+
+}
